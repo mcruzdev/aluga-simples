1671ee9b30eed88046956739c22b0f509ddaf3ee
Matheus Cruz <matheuscruz.dev@gmail.com>
2025-07-14 12:36:19 -0300

    Configure firebase analytics

--- aluga-simples/src/app/app.config.ts
+++ aluga-simples/src/app/app.config.ts
@@ -3,8 +3,12 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 import { provideRouter } from '@angular/router';
+import { } from '@angular/fire'
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAnalytics, provideAnalytics, ScreenTrackingService } from '@angular/fire/analytics';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,6 +18,8 @@ export const appConfig: ApplicationConfig = {
       theme: {
         preset: Aura
       }
-    })
+    }), provideFirebaseApp(() => initializeApp(
+      environment.firebaseConfig
+    )), provideAnalytics(() => getAnalytics()), ScreenTrackingService
   ]
 };
